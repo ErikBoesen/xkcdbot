6ccca6d67b916e07aac06062c68054eaac31a443
ErikBoesen <me@erikboesen.com>
2022-10-18 20:52:37 -0400

    Move bot code into handler.rb

--- handler.rb
+++ handler.rb
@@ -1,40 +1,43 @@
-require "sinatra"
-require "mebots"
-require "json"
-require "net/http"
+require 'mebots'
+require 'json'
+require 'net/http'
 
 def hello(event:, context:)
+  message = JSON.parse(event['body'])
+  responses = process(message)
+  if responses
+    reply(responses, message['group_id'])
+  end
   {
     statusCode: 200,
     body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+      message: 'Message received'
     }.to_json
   }
 end
 
 
-PREFIX = "xkcd"
-BOT = Bot.new("xkcdbot", ENV["BOT_TOKEN"])
-POST_URI = URI("https://api.groupme.com/v3/bots/post")
+PREFIX = 'xkcd'
+BOT = Bot.new('xkcdbot', ENV['BOT_TOKEN'])
+POST_URI = URI('https://api.groupme.com/v3/bots/post')
 POST_HTTP = Net::HTTP.new(POST_URI.host, POST_URI.port)
 POST_HTTP.use_ssl = true
 POST_HTTP.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
 def xkcd_get(query)
-  uri = URI(query != "" ? "https://xkcd.com/#{query}/info.0.json" : "https://xkcd.com/info.0.json")
+  uri = URI(query != '' ? "https://xkcd.com/#{query}/info.0.json" : 'https://xkcd.com/info.0.json')
   response = Net::HTTP.get(uri)
   return JSON.parse(response)
 end
 
 def process(message)
-  text = message["text"].downcase
+  text = message['text'].downcase
   responses = []
-  if message["sender_type"] == "user"
+  if message['sender_type'] == 'user'
     if text.start_with?(PREFIX)
-      text = text.sub(PREFIX, "").strip
+      text = text.sub(PREFIX, '').strip
       comic = xkcd_get(text)
-      responses.push([comic["alt"], comic["img"]])
+      responses.push([comic['alt'], comic['img']])
     end
   end
   return responses
@@ -46,7 +49,7 @@ def reply(message, group_id)
       reply(item, group_id)
     }
   else
-    req = Net::HTTP::Post.new(POST_URI, "Content-Type" => "application/json")
+    req = Net::HTTP::Post.new(POST_URI, 'Content-Type' => 'application/json')
     req.body = {
         bot_id: BOT.instance(group_id).id,
         text: message,
@@ -55,10 +58,5 @@ def reply(message, group_id)
   end
 end
 
-post "/" do
-  message = JSON.parse(request.body.read)
-  responses = process(message)
-  if responses
-    reply(responses, message["group_id"])
-  end
+post '/' do
 end
